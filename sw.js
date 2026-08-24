3be6c011f2387497bbb5acee99bbad3794295276
NOTyeamu <114867406+NOTyeamu@users.noreply.github.com>
2026-08-24 20:38:29 +0400

    feat: device detection with dedicated mobile UI, deeper black theme, white accent, brighter status colors

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='tournament-grid-v3';
+var CACHE='tournament-grid-v4';
 var ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}).then(function(){return self.skipWaiting()}));
